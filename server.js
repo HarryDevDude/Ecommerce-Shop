0bbceb578406b5f2ec5923df5677074a5c87aef9
HarryDevDude <102680902+HarryDevDude@users.noreply.github.com>
2022-05-27 07:36:30 -0400

    added dependencies and images

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+// Middleware
+
+
+
+
+// ROUTE
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
+  res.send('<h1>Hello!</h1>');
 });
+
 
 app.listen(3000, function () {
   console.log('Listening on port 3000');
